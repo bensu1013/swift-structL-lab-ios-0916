7b8bd9f4d338e1865e4da47c4118054c7408dbc4
Ben Su <bensu1013@hotmail.com>
2016-09-30 18:10:04 -0400

    Jim, man, really? really??

--- StructLove/Person.swift
+++ StructLove/Person.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+struct Person {
+    
+    let firstName : String
+    let lastName : String
+    let home : Coordinate
+    var isHungry : Bool
+    
+    init(firstName: String, lastName: String, home: Coordinate) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.home = home
+        self.isHungry = true
+    }
+    
+    mutating func eatPizza(from service: PizzaDeliveryService) -> Bool {
+        
+        if isHungry && service.isInRange(to: home) {
+            isHungry = false
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
